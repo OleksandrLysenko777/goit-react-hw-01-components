bc4b21fde3e6dbdea8c22ff10fc5dc0d0480db5d
OleksandrLysenko777 <114435880+OleksandrLysenko777@users.noreply.github.com>
2023-04-28 19:49:33 +0200

    last

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Profile from './Profile/Profile';
+import user from '../components/Profile/user.json';
+import Statistics from '../components/Statictics/Statistics';
+import statisticalData from './Statictics/data.json';
+import FriendList from '../components/FriendsList/FriendList';
+import friends from './FriendsList/friends.json';
+import TransactionHistory from '../components/Transaction/TransactionHistory';
+import transactions from './Transaction/transactions.json';
+
+const App = () => (
+  <>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={statisticalData} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
+  </>
+);
+
+export default App;
